c3ece751f142f9d03f145989709f198ff8b19cb5
Hieu Tran <hieutv-dng@github.com>
2024-09-25 18:54:05 +0700

    Nộp bài tập 1

---
+++ baitap01/bin/baitap01.dart
@@ -0,0 +1,5 @@
+import 'package:baitap01/baitap01.dart' as baitap01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${baitap01.calculate()}!');
+}
---
+++ baitap01/test/baitap01_test.dart
@@ -0,0 +1,8 @@
+import 'package:baitap01/baitap01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
